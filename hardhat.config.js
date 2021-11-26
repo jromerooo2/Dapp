992c017845ed365704ea4273a1c1b7bc85a5c8ac
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-11-25 21:42:54 -0600

    some changes :)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 
-const RINKEBY_RPC_URL = process.env.INFURA;
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
